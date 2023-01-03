50ef3a1c27c1135532b7b5e073760d8811109003
Damir Mandakovic <damir@notus.cl>
2023-01-03 19:06:49 -0300

    media folder for images

--- adeback/urls.py
+++ adeback/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from cerros.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', IndexView.as_view(), name='index'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
