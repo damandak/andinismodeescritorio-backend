94a50280956ffc2527ae5392158e592d50528113
Damir Mandakovic <damir@notus.cl>
2023-01-18 15:04:30 -0300

    composary email field

--- cerros/admin.py
+++ cerros/admin.py
@@ -53,6 +53,5 @@ class CustomUserAdmin(admin.ModelAdmin):
   list_display = ('email', 'first_name', 'last_name', 'is_superuser')
   search_fields = ('email', 'first_name', 'last_name')
   list_filter = ('is_superuser',)
-  readonly_fields = ('email',)
     
 admin.site.register(CustomUser, CustomUserAdmin)
