4f9b2cdb8a31cbc6556a848139bcb04e4a3d2052
Damir Mandakovic <damir@notus.cl>
2023-01-18 13:49:25 -0300

    jazzmin admin

--- cerros/admin.py
+++ cerros/admin.py
@@ -18,7 +18,7 @@ from .models import (
 
 # Register your models here.
 class MountainAdmin(admin.ModelAdmin):
-  list_display = ('id', 'prefix', 'name', 'altitude')
+  list_display = ('id', 'prefix', 'name', 'altitude', 'ascended', 'first_absolute', 'main_image')
   search_fields = ('id', 'prefix__prefix', 'name', 'altitude')
 
 admin.site.register(Mountain, MountainAdmin)
@@ -29,15 +29,30 @@ admin.site.register(Country)
 admin.site.register(Region)
 admin.site.register(NomenclaturaSummit)
 admin.site.register(IGMMap)
-admin.site.register(Route)
+
+class RouteAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'mountain')
+  search_fields = ('id', 'name', 'mountain__name', 'mountain__prefix__prefix')
+admin.site.register(Route, RouteAdmin)
 
 class AndinistAdmin(admin.ModelAdmin):
   list_display = ('id', 'name', 'surname')
   search_fields = ('id', 'name', 'surname', 'clubs__name')
 admin.site.register(Andinist, AndinistAdmin)
 
-admin.site.register(Club)
-admin.site.register(Ascent)
+class ClubAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'shortname')
+  search_fields = ('id', 'name')
+admin.site.register(Club, ClubAdmin)
+
+class AscentAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'route', 'mountain', 'date')
+  search_fields = ('id', 'name', 'route__name', 'route__mountain__name', 'route__mountain__prefix__prefix')
+  @admin.display(description='Mountain')
+  def mountain(self, obj):
+    return obj.route.mountain
+admin.site.register(Ascent, AscentAdmin)
+
 admin.site.register(Reference)
 
 class ImageAdmin(admin.ModelAdmin):
